f9ac32ef526319cd0d122e3cfa2d96639b4b1476
Pierre Caretero <pierre.caretero@ruedesecoles.com>
2023-04-20 19:15:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+
+const items = ["Spirit Box","Livre d'écriture fantomatique","Lecteur EMF","Lampe UV","Lampe de poche","Caméra vidéo","Projecteur D.O.T.S.","Bougie","Crucifix","Bâton lumineux","Caméra frontale","Détecteur de mouvement","Microphone parabolique","Sel","Pilules calmantes","Bâton d'encens","Capteur sonore","Lampe de poche puissante","Thermomètre","Trépied"]
+
+let itemsRemaining = [];
+const client = new tmi.Client({
+    options: { debug: true, messagesLogLevel: "info" },
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+
+    // Lack of the identity tags makes the bot anonymous and able to fetch messages from the channel
+    // for reading, supervision, spying, or viewing purposes only
+    identity: {
+        username: `${process.env.TWITCH_USERNAME}`,
+        password: `oauth:${process.env.TWITCH_OAUTH}`
+    },
+    channels: [`${process.env.TWITCH_CHANNEL}`]
+});
+
+
+client.connect().catch(console.error);
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+client.on('message', (channel, tags, message, self) => {
+    if (self) return;
+
+console.log(tags);
+if (tags.vip || tags.mod || tags.badges.broadcaster) {
+    switch (message.toLowerCase()) {
+        case '!newgame':
+            client.say(channel, 'Une nouvelle partie de Photo surprise a été lancée');
+            itemsRemaining = items.slice(0);
+            console.log(itemsRemaining);
+            break;
+        case '!objet':
+            shuffleArray(itemsRemaining);
+            let item = itemsRemaining.pop();
+            client.say(channel, 'Le nouvel objet est : ' + item);
+            console.log(itemsRemaining);
+            break;
+        default:
+            break;
+    }
+}
+
+});
